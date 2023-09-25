703f8eea002269b629ff765f8a2e865da3419c10
sandu <badanausandu@gmail.com>
2023-09-25 12:05:38 +0300

    first coommit

---
+++ index.js
@@ -0,0 +1,93 @@
+// stiluri - camel case, upper case, upper snake case
+
+var camelCase = 'camelCase';
+var UPPER_SNAKE_CASE = 'upper snake';
+var USERS_LIMIT_PER_PAGE = 'user snake case'
+
+
+var str1 = 'str1'
+var str2 = 'str2'
+var str3 = 'str3'
+
+// transformari 
+
+const string = '2'
+const number = 2
+
+
+
+
+
+
+
+// falsy conditions
+
+const condition = true;
+const falsy1 = 0;
+const falsy2 = '';
+const falsy3 = undefined;
+const falsy4 = null;
+const falsy5 = NaN;
+
+
+
+if (falsy1) {
+    console.log('pass')
+} else {
+    console.log('do not pass')
+}
+if (falsy2) {
+    console.log('pass')
+} else {
+    console.log('do not pass')
+}
+if (falsy3) {
+    console.log('pass')
+} else {
+    console.log('do not pass')
+}
+if (falsy4) {
+    console.log('pass')
+} else {
+    console.log('do not pass')
+}
+if (falsy5) {
+    console.log('pass')
+} else {
+    console.log('do not pass')
+}
+
+
+console.log("exercitiul !, ||, &")
+
+// negatia, operatorii or si and
+
+
+var conditia1 = 0
+var conditia2 = 1
+
+if (conditia1 || conditia2) {
+    console.log('pass')
+}
+
+if (conditia1 && conditia2) {
+    console.log('pass')
+}
+
+if (!conditia1 || conditia2) {
+    console.log('pass')
+} console.log('do not pass')
+
+
+
+
+console.log(`${str1} ${str2} lala lala lala ${str3}`)
+
+console.log(typeof +string)
+console.log(typeof Number(string))
+console.log(typeof Boolean(string))
+
+console.log(typeof number.toString())
+console.log(typeof Boolean(number))
+
+
